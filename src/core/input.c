67324d84fbe88bd856cca30e8a186237f1d41168
aceinetx <mouselin82@gmail.com>
2025-10-01 22:02:00 +0300

    full widechar support

--- src/core/input.c
+++ src/core/input.c
@@ -6,12 +6,29 @@
 #include "core/cursor.h"
 #include "core/state.h"
 
-bool Input_IsTextChar(int k) {
-    return (k >= 'a' && k <= 'z') || (k >= 'A' && k <= 'Z') || k == '!'
-        || k == '.' || k == '-' || k == '+' || k == ' ';
+bool Input_IsTextChar(wchar_t k) {
+    size_t i, ranges_total;
+    const wchar_t ranges[][2] = {
+        {L'a', L'z'},
+        {L'A', L'Z'},
+        {L'а', L'я'},
+        {L'А', L'Я'},
+        {L'!', L'!'},
+        {L'.', L'.'},
+        {L',', L','},
+        {L' ', L' '},
+    };
+    ranges_total = sizeof(ranges) / sizeof(ranges[0]);
+    for (i = 0; i < ranges_total; i++) {
+        const wchar_t* range = ranges[i];
+        if (k >= range[0] && k <= range[1]) {
+            return true;
+        }
+    }
+    return false;
 }
 
-void Input_InputChar(int k) {
+void Input_InputChar(wchar_t k) {
     wchar_t *c, *pos = NULL;
     size_t line = 0, line_char = 0, pos_index = 0;
 
@@ -36,7 +53,7 @@ void Input_InputChar(int k) {
 
     if (pos) {
         pos_index = pos - state.buffer;
-        Buffer_InsertChar(pos_index, (char)k);
+        Buffer_InsertChar(pos_index, k);
         Cursor_MoveRight();
     }
 }
